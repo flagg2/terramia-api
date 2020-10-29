4dea8ac3c361cf50776a4b6990b1275dc8b01538
flagg2 <samo.wittlinger@gmail.com>
2020-10-29 16:36:37 +0100

    cors fix

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use(bodyParser.json({
 app.use('*', (req,res,next) => {
     res.header('Access-Control-Allow-Origin',"*")
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Headers' ,'Origin, Content-Type, X-Auth-Token')
+    res.header('Access-Control-Allow-Headers' ,'*')
+    res.header('Access-Control-Expose-Headers', '*')
     next()
 })
 app.use('/uploads',express.static('./uploads'))
