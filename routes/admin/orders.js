bb232a1229a86f4488fd877b0e4b66e7e2eaa89d
flagg2 <samo.wittlinger@gmail.com>
2020-11-23 14:39:22 +0100

    new methods

--- routes/admin/orders.js
+++ routes/admin/orders.js
@@ -62,10 +62,31 @@ router.post('/orders/:id/fulfill',verify(1),async (req,res) => {
         if (!order) return notFound(res,'Order')
         if (order.status == 'pending') return res.status(400).send({message:'Cannot fulfill an unpaid order',error:'unpaid'})
         if (order.status == 'fulfilled') return res.status(400).send({message:'The order has already been fulfileld',error:'fulfilled'})
+        if (order.status == 'cancelled') return res.status(400).send({message:'The order has already been cancelled',error:'cancelled'})
         order.status = 'fulfilled'
         await order.save()
         res.send({
             message:'Order fulfilled successfully',
+            order:order
+        })
+    }
+    catch(err){
+        serverError(res,err)
+    }
+})
+router.all('/orders/:id/cancel',methods(['POST']))
+router.post('/orders/:id/cancel',verify(1),async (req,res) => {
+    if (idValidation(req,res)) return
+    try{
+        const order = await Order.findById(req.params.id)
+        if (!order) return notFound(res,'Order')
+        if (order.status == 'pending') return res.status(400).send({message:'Cannot cancel an unpaid order',error:'unpaid'})
+        if (order.status == 'fulfilled') return res.status(400).send({message:'The order has already been fulfilled',error:'fulfilled'})
+        if (order.status == 'cancelled') return res.status(400).send({message:'The order has already been cancelled',error:'cancelled'})
+        order.status = 'cancel'
+        await order.save()
+        res.send({
+            message:'Order cancelled successfully',
             order:order
         })
     }
