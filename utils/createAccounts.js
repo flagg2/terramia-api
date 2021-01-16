35c14bf1106fdddb2a44b308873528684ead7366
flagg2 <samo.wittlinger@gmail.com>
2021-01-16 13:15:35 +0100

    Getting ready for deployment

---
+++ utils/createAccounts.js
@@ -0,0 +1,36 @@
+const User = require('../model/user')
+const {sendWelcomeEmail} = require('../utils/mailer')
+const bcrypt = require('bcryptjs')
+const fs = require('fs')
+const crypto = require('crypto')
+const emailArray = []
+const sendEmail = async ()=>{
+  try {
+    const data = fs.readFileSync('./utils/test.csv', 'utf8')
+    console.log(data)
+    const lines = data.split('\n')
+    for (line of lines){
+      const [v1,v2,v3] = line.split(',')
+      if (v1) emailArray.push(v1)
+      if (v2) emailArray.push(v2)
+      if (v3) emailArray.push(v3)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  for (email of emailArray){
+    console.log('Now sending to '+email)
+    const pwd = crypto.randomBytes(20).toString('hex')
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(pwd, salt)
+    const user = new User({
+      email:email,
+      password:hashPassword
+    })
+    await user.save()
+    await sendWelcomeEmail(email,pwd)
+  
+  }
+}
+
+sendEmail()
