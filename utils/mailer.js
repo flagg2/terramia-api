59f445bd4033e4bfa9d136f1f5b8aff821aecc27
flagg2 <samo.wittlinger@gmail.com>
2021-01-16 14:51:02 +0100

    Fixed link

--- utils/mailer.js
+++ utils/mailer.js
@@ -59,7 +59,7 @@ var readHTMLFile = (path, callback) => {
 
 const sendWelcomeEmail = async (receiverAdress, pwd) => {
     const transporter = await createTransport()
-    const link = `terramia.sk/auth/${receiverAdress}/${pwd}`
+    const link = `https://terramia.sk/auth/${receiverAdress}/${pwd}`
     readHTMLFile('./email_content/welcome.html', function (err, html) {
         var template = handlebars.compile(html);
         var replacements = {
