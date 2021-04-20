5e5f26e66a7c87d4621375b0e5b17a2bd47acdef
flagg2 <samo.wittlinger@gmail.com>
2021-04-20 16:45:08 +0200

    Bugfixes

--- routes/store/products.js
+++ routes/store/products.js
@@ -41,7 +41,7 @@ module.exports = (router) => {
             const products = await Product.find({
                 ...req.body.filters,
                 name: {$not: {$in: ['Doprava','Dobierka','Doprava2']}},
-            }).limit(req.body.limit).sort(sortBy)
+            }).limit(req.body.limit).sort(req.body.sortBy)
             if (req.body.query) {
                 const searchResults = smartSearch(req.body.query, products)
                 return res.send({
