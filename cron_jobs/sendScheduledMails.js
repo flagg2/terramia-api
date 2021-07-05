4ec013606c55c488b9152180a28216bd7fb0bdb3
Samuel Wittlinger <samo.wittlinger@gmail.com>
2021-07-05 13:02:00 +0200

    testing

--- cron_jobs/sendScheduledMails.js
+++ cron_jobs/sendScheduledMails.js
@@ -2,24 +2,27 @@ const CronJob = require("cron").CronJob;
 const User = require("../model/user");
 const moment = require("moment");
 const tm = require("../utils/templateMailer");
-const job = new CronJob("0 17 */1 * *", async function () {
-   const users = await User.find({
-      tempUser: false,
-      scheduledMails: {
-         $exists: true,
-      },
-   });
-   const today = moment().format("DD/MM/YYYY");
-   for (const user of users) {
-      if (user.scheduledMails[today]) {
-         for (const mail of user.scheduledMails[today]) {
-            await tm.sendTemplate({
-               name: mail,
-               email: user.email,
-            });
-            console.log(`Sending email to ${user.email}`);
+const job = new CronJob(
+   "0 17 */1 * *",
+   (async function () {
+      const users = await User.find({
+         tempUser: false,
+         scheduledMails: {
+            $exists: true,
+         },
+      });
+      const today = moment().format("DD/MM/YYYY");
+      for (const user of users) {
+         if (user.scheduledMails[today]) {
+            for (const mail of user.scheduledMails[today]) {
+               await tm.sendTemplate({
+                  name: mail,
+                  email: user.email,
+               });
+               console.log(`Sending email to ${user.email}`);
+            }
          }
       }
-   }
-})();
+   })()
+);
 job.start();
